7f7fe4a156600295c6d0638db8297b4cbe130160
chiragpatel1229 <94766490+chiragpatel1229@users.noreply.github.com>
2022-11-28 16:01:00 +0100

    application is working okay

--- Analyzer_EXA_N9010A.m
+++ Analyzer_EXA_N9010A.m
@@ -3,13 +3,15 @@
 
 %  obj = Analyzer_EXA_N9010A()          Connect
 %  obj.Auto_Tune()                      Auto tune the frequency(Spectrogram) window in case of errors
-%  obj.Set_BnadWidth(3e6)               set BW to 3MHz
+%  obj.Mode_Preset()                    Mode Preset (Returns the active mode to a known state) 
 %  obj.Set_Center_Frequency(3.5e9)      set the center frequency to 1GHz
-%  obj.Set_Span(10e9)                   Set Span size to 10Mz
+%  obj.Set_BnadWidth(3.0e6)             set BW to 3MHz
+%  obj.Set_Span(7.0e9)                  Set Span size to 10Mz
 %  obj.Set_Marker_onPeak(1)             set marker on a peak(constant peak search)
+%  obj.Peak_Search(1)                   Peak Search (Sending this command selects the subopcoded marker)
 %  obj.Set_Marker(1)                    put the marker on the screen
 %  obj.Turn_off_Marker(1)               remove the marker from the screen
-%  obj.Turn_off_All_Marker()            remove all the markers from the screen 
+%  obj.Turn_off_All_Markers()            remove all the markers from the screen 
 %  obj.Next_Position(1)                 Move marker to the next position
 %  obj.Get_Marker_Power(1)              Get the power value of marker1
 %  obj.Get_Marker_Freq(1)               Get the frequency value of Marker1
@@ -42,6 +44,12 @@ classdef Analyzer_EXA_N9010A
             command0 = sprintf(':FREQuency:TUNE:IMMediate');
             fprintf(obj.interface, command0);
         end
+        
+     %! Mode Preset (Returns the active mode to a known state)          
+        function Mode_Preset(obj)
+            command0 = sprintf(':SYSTem:PRESet');
+            fprintf(obj.interface, command0);
+        end        
                 
      %! set BW
         function Set_BnadWidth(obj, fre)
@@ -51,6 +59,7 @@ classdef Analyzer_EXA_N9010A
         
      %! set center Frequecy        
         function Set_Center_Frequency(obj, fre)
+            
             command0 = sprintf(':FREQuency:CENTer %eHz', fre);
             fprintf(obj.interface, command0);
         end
@@ -66,7 +75,13 @@ classdef Analyzer_EXA_N9010A
             command0 = sprintf(':Calculate:Marker%d:CPSearch 1', id);
             fprintf(obj.interface, command0);
         end
-                
+        
+     %! Peak Search (Sending this command selects the subopcoded marker)        
+        function Peak_Search(obj, id)
+            command0 = sprintf(':Calculate:Marker%d:MAXimum', id);
+            fprintf(obj.interface, command0);
+        end        
+                        
      %! turn On the marker (number of markers are possible just change the number in the bracket)         
         function Set_Marker(obj, id)      % select the marker number (1,2,3.....)
             command0 = sprintf(':Calculate:Marker%d:STATe 1', id);
@@ -80,16 +95,16 @@ classdef Analyzer_EXA_N9010A
         end        
         
      %! All markers OFF          
-        function Turn_off_All_Marker(obj)
+        function Turn_off_All_Markers(obj)
             command0 = sprintf(':Calculate:Marker:AOFF');
             fprintf(obj.interface, command0);
         end        
         
       %! move A marker to the Next Position
-           function Next_Position(obj, id)
-                command0 = sprintf(':CALCulate:Marker%d:MAXimum:NEXT:', id);
-                fprintf(obj.interface, command0);
-           end
+         function Next_Position(obj, id)
+            command0 = sprintf(':CALCulate:Marker%d:MAXimum:NEXT', id);
+            fprintf(obj.interface, command0);
+         end
            
 %       %! move the marker Next Right Position
 %            function Next_Peake_Right(obj, id)
