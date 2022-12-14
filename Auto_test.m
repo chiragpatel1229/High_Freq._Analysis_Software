d41a2bbb041098f7ba061cdefee6897585a55f5b
chiragpatel1229 <94766490+chiragpatel1229@users.noreply.github.com>
2022-12-14 17:30:15 +0100

    14-12-2022

--- Auto_test.m
+++ Auto_test.m
@@ -10,19 +10,19 @@
 % end
 
 
-function Auto_test (times, degrees, seconds)
-obj = Analyzer_EXA_N9010A();
-a = [];
-b = [];
-
-for step_times = 1:times
-    Step_Size = step_times * degrees;
-    MyArcus.PositionTo(Step_Size);
-    pause(1.5)
-    a = obj.Get_Marker_Power(1)
-    b = obj.Get_Marker_Freq(1)
-    pause (seconds)
-end
-pause(2);
-MyArcus.PositionTo(0);
-end
+function Auto_test(times, degrees, seconds)
+            app.obj = Analyzer_EXA_N9010A();
+            
+            H = table([],[],[],'VariableNames', ["Step_Size", "Power", "Freq"]);
+            app.UITable = [];
+            for step_times = 1:times 
+                Step_Size = step_times * degrees;
+                MyArcus.PositionTo(Step_Size);
+                Power = str2double(app.obj.Get_Marker_Power(1));
+                Freq  = str2double(app.obj.Get_Marker_Freq(1));
+                H(end+1,:) = {Step_Size, Power, Freq} % Add new rows of data in the loop
+                pause(seconds);
+            end
+            pause(2);
+            MyArcus.PositionTo(0);
+        end 
