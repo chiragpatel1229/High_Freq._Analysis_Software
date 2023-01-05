7bdd88ac7ecb712bcc687b29857312dfd7f6ce88
chiragpatel1229 <94766490+chiragpatel1229@users.noreply.github.com>
2023-01-05 11:53:04 +0100

    -

---
+++ Table.m
@@ -0,0 +1,21 @@
+a = readtable('360-3-2.xlsx');
+b = readtable('Yagi.xlsx');
+x=table2array(a(:,2));
+y=table2array(a(:,3));
+e=table2array(b(:,2));
+f=table2array(b(:,3));
+x1 = deg2rad(x);
+e1 = deg2rad(e);
+figure(1)
+polarplot(x1, y)
+% figure(2)  
+hold on
+polarplot(e1, f)
+hold off
+
+
+
+%% ...
+%2021a model does not require any specific data type for the polar diagram 
+% BUT 2022b does need to have a specific datatype.
+% smaple code rushabh na email ma che \ 
