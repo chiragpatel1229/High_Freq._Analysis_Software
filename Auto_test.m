67dfd1f4c12deb461cfe3463e116d788349bb705
chiragpatel1229 <94766490+chiragpatel1229@users.noreply.github.com>
2023-01-09 12:02:55 +0100

    08-01-20223

--- Auto_test.m
+++ Auto_test.m
@@ -3,43 +3,28 @@
 function T = Auto_test(range, resolution, seconds)
             app.obj = Analyzer_EXA_N9010A(); 
             app.obj.Set_Marker_onPeak(1)
+
             times = range / resolution;
+
             C = [times 4];
             J = {'string','double','double','double'};
             P = ["No.","Step_Size","Power","Freq"];
             T =  table('Size',C,'VariableTypes',J,'VariableNames',P);
-            for step_times = 1:times 
+
+            for step_times = 1:(times + 1) 
+
+                Step_Size = (step_times - 1) * resolution;
+
+                MyArcus.PositionTo(Step_Size);
                 pause (seconds)
+
                 Power = str2double(app.obj.Get_Marker_Power(1));
                 Freq  = str2double(app.obj.Get_Marker_Freq(1));
-                Step_Size = step_times * resolution;
-                MyArcus.PositionTo(Step_Size);
-                T(step_times,:) =  {step_times,Step_Size, Power, Freq}
+                
+                T(step_times,:) = {step_times,Step_Size, Power, Freq}
                 pause(0.5)
                 
             end 
             pause (1)
             MyArcus.PositionTo(0);
-end 
-
-        
-
-% 
-% function a = Auto_test(times, resolution, ~)
-%             step_times = 0;
-%             while step_times<times
-%                 step_times = step_times+1;
-%                 Step_Size = step_times * resolution;
-%                 MyArcus.PositionTo(Step_Size);
-%                 try
-%                    a = MyArcus.IsBusy();
-%                    if strcmp(a,'1')
-%                    end         
-%                 catch
-%                      warning('The Motor is busy!!');
-%                 end
-%                 
-% %                 pause(seconds);
-%             end 
-%             MyArcus.PositionTo(0);
-% end 
+end
--- Table.m
+++ Table.m
@@ -18,10 +18,63 @@ polarplot(x1, y)
 % hold on
 % polarplot(e1, f)
 % hold off
+% 
+% 
+% 
+% %% ...
+% %2021a model does not require any specific data type for the polar diagram 
+% % BUT 2022b does need to have a specific datatype.
+% % smaple code rushabh na email ma che \ 
+
+% Set up the polar plot
+% h = polarplot(0,0);
+
+% % Set the axis limits and label properties
+% rlim([0 1]);
+% rtickformat('%d%%');
+% thetalim([0 360]);
+% thetaticks(0:45:360);
+% thetatickformat('%d^o');
+% 
+% % Set the starting angle and radius
+% angle = 15;
+% radius = 56;
+% 
+% % Set the loop counter
+% n = 1;
+% 
+% % Start the loop
+% while n <= 100
+%     % Update the angle and radius
+%     angle = angle + 3;
+%     radius = sin(n/10);
+%     
+%     % Update the polar plot data
+% %     h.RData = radius;
+% %     h.ThetaData = angle;
+%      polarplot(radius,angle)
+%     
+%     % Refresh the plot
+%     drawnow
+%     
+%     % Increment the loop counter
+%     n = n + 1;
+% end
 
 
 
-%% ...
-%2021a model does not require any specific data type for the polar diagram 
-% BUT 2022b does need to have a specific datatype.
-% smaple code rushabh na email ma che \ 
+% closing request for app designer
+
+            selection = uiconfirm(app.AntennaTestbedUIFigure,...
+                ["Closing App?";"Please reset and disconnect EXA & MXG."],...
+                'Confirm Close', 'Options',{'Disconnect EXA & MXG','OK','Cancel'});
+            switch selection
+                case 'OK'
+                    delete(app)
+                case 'Disconnect EXA & MXG'
+                    app.obj1.Disconnect_Generator();
+                    app.obj.Disconnect_Analyzer();
+                case 'Cancel'
+                    return
+            end
+        end
